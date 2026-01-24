471adba865742a4bfaab26d90c13353922536ed1
KLTPL <kltpl@duck.com>
2026-01-24 21:21:13 +0100

    Initial commit - basic setup
    Everything I did since the LazyVim installation.
    Added Hardtime and Precognition plugins for learingn purposes.
    Noting else realy.

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  event = "VeryLazy",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  keys = { "<leader>co", "<cmd>Hardtime toggle<cr>", desc = "Toggle hardtime" },
+  opts = {},
+}
