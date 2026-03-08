d71a14cbe4387e6d4549a0c974decc8cec945fd1
KLTPL <kltpl@duck.com>
2026-03-08 01:05:53 +0100

    Added keymaps so deleting does not result in cutting text (delete instead of copy and delete)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Never copy when deleting with 'd' or 'x'
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete to end of line without yanking" })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete character without yanking" })
+vim.keymap.set({ "n", "v" }, "x", '"_X', { desc = "Delete character backwords without yanking" })
+
+-- Paste over currently selected text without yanking it
+vim.keymap.set("v", "p", '"_dp', { desc = "Paste without replacing clipboard" })
